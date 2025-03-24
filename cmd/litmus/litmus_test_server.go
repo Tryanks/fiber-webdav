2514cc99a0c8467437b08fa1b835eab065d5490d
Tryanks <tryanks@outlook.com>
2025-03-24 13:06:39 +0800

    Remove build constraint from litmus_test_server.go

--- cmd/litmus/litmus_test_server.go
+++ cmd/litmus/litmus_test_server.go
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build ignore
-
 /*
 This program is a server for the WebDAV 'litmus' compliance test at
 http://www.webdav.org/neon/litmus/
